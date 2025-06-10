d90f317fafe6d9d0740466964bedd310188aa6ed
Mauricio Noriega <mauricionoriega.sw@outlook.com>
2025-06-09 23:57:04 -0600

    Add: vistas y controladores para CU
    Añadidas archivos FXML y clases Controller para preparar la implementación de los casos de uso.

---
+++ PracticasProfesionalesLIS/src/practicasprofesionaleslis/controlador/coordinador/FXMLBusquedaEstudianteController.java
@@ -0,0 +1,14 @@
+package practicasprofesionaleslis.controlador.coordinador;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+public class FXMLBusquedaEstudianteController implements Initializable {
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
---
+++ PracticasProfesionalesLIS/src/practicasprofesionaleslis/controlador/profesoree/FXMLBuscarEstudianteController.java
@@ -0,0 +1,14 @@
+package practicasprofesionaleslis.controlador.profesoree;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+public class FXMLBuscarEstudianteController implements Initializable {
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
