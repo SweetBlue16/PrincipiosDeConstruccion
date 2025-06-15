a54b2bffd9245857536f898ad52781f95f7df035
Yael Castillo <yael.castillo.dev@gmail.com>
2025-06-15 06:46:23 -0600

    add: Ventana buscar Estudiante
    Nota: cambiar los nombres de clases de verbo a sustantivo

--- PracticasProfesionalesLIS/src/practicasprofesionaleslis/controlador/profesoree/FXMLBuscarEstudianteController.java
+++ PracticasProfesionalesLIS/src/practicasprofesionaleslis/controlador/profesoree/FXMLBuscarEstudianteController.java
@@ -17,6 +17,7 @@ import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.AnchorPane;
+import practicasprofesionaleslis.modelo.dao.EstudianteDAO;
 import practicasprofesionaleslis.modelo.dao.ExpedienteDAO;
 import practicasprofesionaleslis.modelo.pojo.Estudiante;
 import practicasprofesionaleslis.modelo.pojo.ExperienciaEducativa;
@@ -28,16 +29,19 @@ public class FXMLBuscarEstudianteController implements Initializable {
     private AnchorPane apBuscarEstudiante;
     @FXML
     private TableView<Estudiante> tvEstudiantes;
-    @FXML
-    private TableColumn<Estudiante, String> colNombreCompleto;
-    
     private ObservableList<Estudiante> estudiantes;
-    
     private ExperienciaEducativa experienciaEducativa;
+    
     @FXML
     private Label lblEstudiantes;
     @FXML
     private Label lbltitulo;
+    @FXML
+    private TableColumn<Estudiante, String> colNombre;
+    @FXML
+    private TableColumn<Estudiante, String> colApellidoPaterno;
+    @FXML
+    private TableColumn<Estudiante, String> colApellidoMaterno;
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
@@ -47,19 +51,20 @@ public class FXMLBuscarEstudianteController implements Initializable {
     public void inicializarDatos(ExperienciaEducativa experienciaEducativa) {
         this.experienciaEducativa = experienciaEducativa;
         if (experienciaEducativa != null) {
-            lbltitulo.setText("Estudiantes de " + experienciaEducativa.getNombre());
             cargarEstudiantes();
         }
     }
 
     private void configurarTabla() {
-        colNombreCompleto.setCellValueFactory(new PropertyValueFactory<>("nombreCompleto"));
+        colNombre.setCellValueFactory(new PropertyValueFactory<>("nombre"));
+        colApellidoPaterno.setCellValueFactory(new PropertyValueFactory<>("apellidoPaterno"));
+        colApellidoMaterno.setCellValueFactory(new PropertyValueFactory<>("apellidoMaterno"));
     }
     
     private void cargarEstudiantes() {
         try {
             estudiantes = FXCollections.observableArrayList();
-            List<Estudiante> estudiantesDAO = ExpedienteDAO.obtenerEstudiantesPorExperienciaEducativa(experienciaEducativa.getId());
+            List<Estudiante> estudiantesDAO = EstudianteDAO.obtenerEstudiantesPorExperienciaEducativa(experienciaEducativa.getId());
             estudiantes.addAll(estudiantesDAO);
             tvEstudiantes.setItems(estudiantes);
         } catch (SQLException e) {
@@ -74,5 +79,9 @@ public class FXMLBuscarEstudianteController implements Initializable {
     @FXML
     private void clicBtnCancelar(ActionEvent event) {
         VentanasUtils.cerrarVentana(lbltitulo);
+    }
+
+    @FXML
+    private void clicBtnSeleccionar(ActionEvent event) {
     }
 }
