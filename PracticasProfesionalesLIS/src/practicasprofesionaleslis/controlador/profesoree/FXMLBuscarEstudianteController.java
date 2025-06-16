8db867c785dfe47dfbb84fa56b3a21acfd291880
Yael Castillo <yael.castillo.dev@gmail.com>
2025-06-16 02:33:42 -0600

    update: controllers para escoger estudiante de experiencia educativa

--- PracticasProfesionalesLIS/src/practicasprofesionaleslis/controlador/profesoree/FXMLBuscarEstudianteController.java
+++ PracticasProfesionalesLIS/src/practicasprofesionaleslis/controlador/profesoree/FXMLBuscarEstudianteController.java
@@ -9,7 +9,10 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -17,9 +20,11 @@ import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 import practicasprofesionaleslis.modelo.dao.EstudianteDAO;
 import practicasprofesionaleslis.modelo.dao.ExpedienteDAO;
 import practicasprofesionaleslis.modelo.pojo.Estudiante;
+import practicasprofesionaleslis.modelo.pojo.Expediente;
 import practicasprofesionaleslis.modelo.pojo.ExperienciaEducativa;
 import practicasprofesionaleslis.utilidades.ConstantesUtils;
 import practicasprofesionaleslis.utilidades.VentanasUtils;
@@ -83,5 +88,41 @@ public class FXMLBuscarEstudianteController implements Initializable {
 
     @FXML
     private void clicBtnSeleccionar(ActionEvent event) {
+        Estudiante estudianteSeleccionado = tvEstudiantes.getSelectionModel().getSelectedItem();
+        if (estudianteSeleccionado != null) {
+            try {
+                // Get the expediente for the selected student
+                Expediente expediente = ExpedienteDAO.obtenerExpedienteActivoPorEstudiante(estudianteSeleccionado.getId());
+
+                if (expediente != null) {
+                    // Load the DocumentosEntregados view
+            FXMLLoader loader = new FXMLLoader(getClass().getResource(
+                "practicasprofesionaleslis/vista/profesoree/FXMLDocumentosEntregados.fxml"));
+                            Parent root = loader.load();
+
+                    // Get the controller and initialize data
+                    FXMLDocumentosEntregadosController controller = loader.getController();
+                    controller.inicializarDatos(experienciaEducativa, estudianteSeleccionado, expediente);
+
+                    // Show the new window
+                    Stage stage = new Stage();
+                    stage.setScene(new Scene(root));
+                    stage.show();
+                } else {
+                    VentanasUtils.mostrarAlertaSimple(Alert.AlertType.WARNING,
+                        "No hay expediente",
+                        "El estudiante seleccionado no tiene un expediente activo");
+                }
+            } catch (SQLException | IOException e) {
+                e.printStackTrace();
+                VentanasUtils.mostrarAlertaSimple(Alert.AlertType.ERROR,
+                    "Error",
+                    "No se pudo cargar la información del estudiante");
+            }
+        } else {
+            VentanasUtils.mostrarAlertaSimple(Alert.AlertType.WARNING,
+                "Selección requerida",
+                "Debe seleccionar un estudiante de la tabla");
+        }
     }
 }
--- PracticasProfesionalesLIS/src/practicasprofesionaleslis/controlador/profesoree/FXMLDocumentosEntregadosController.java
+++ PracticasProfesionalesLIS/src/practicasprofesionaleslis/controlador/profesoree/FXMLDocumentosEntregadosController.java
@@ -1,20 +1,105 @@
 package practicasprofesionaleslis.controlador.profesoree;
 
+import java.io.IOException;
 import java.net.URL;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+//import javafx.scene.control.cell.PropertyValueFactory;
+//import practicasprofesionaleslis.modelo.dao.EntregaDAO;
+import practicasprofesionaleslis.modelo.dao.EntregaDocumentoFinalDAO;
+import practicasprofesionaleslis.modelo.dao.EntregaDocumentoInicialDAO;
+import practicasprofesionaleslis.modelo.dao.EntregaDocumentoIntermedioDAO;
+import practicasprofesionaleslis.modelo.dao.EntregaReporteDAO;
+
+
+import practicasprofesionaleslis.modelo.dao.ExpedienteDAO;
+import practicasprofesionaleslis.modelo.pojo.Estudiante;
+import practicasprofesionaleslis.modelo.pojo.Expediente;
+import practicasprofesionaleslis.modelo.pojo.ExperienciaEducativa;
+import practicasprofesionaleslis.modelo.pojo.Entrega;
+import practicasprofesionaleslis.modelo.pojo.EntregaDocumentoFinal;
+import practicasprofesionaleslis.modelo.pojo.EntregaDocumentoInicial;
+import practicasprofesionaleslis.modelo.pojo.EntregaDocumentoIntermedio;
+import practicasprofesionaleslis.modelo.pojo.EntregaReporte;
+import practicasprofesionaleslis.utilidades.ConstantesUtils;
+import practicasprofesionaleslis.utilidades.VentanasUtils;
 
 public class FXMLDocumentosEntregadosController implements Initializable {
 
+    @FXML
+    private Label lblTitulo;
+    @FXML
+    private TableView<Entrega> tbldocumentosEntregados;
+    @FXML
+    private TableColumn<Entrega, String> colNombre;
+    @FXML
+    private TableColumn<Entrega, String> colInicia;
+    @FXML
+    private TableColumn<Entrega, String> colTermina;
+    @FXML
+    private TableColumn<Entrega, String> colPuntaje;
+    
+    private ObservableList<Entrega> entregas;
+    private ExperienciaEducativa experienciaEducativa;
+    private Estudiante estudiante;
+    private Expediente expediente;
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
+        configurarTabla();
+    }
+    
+    public void inicializarDatos(ExperienciaEducativa experienciaEducativa, Estudiante estudiante, Expediente expediente) {
+        this.experienciaEducativa = experienciaEducativa;
+        this.estudiante = estudiante;
+        this.expediente = expediente;
+        
+        cargarEntregas();
+    }
+    
+    private void configurarTabla() {
+       // colNombre.setCellValueFactory(cellData -> cellData.getValue().getNombreProperty());
+    //    colInicia.setCellValueFactory(cellData -> cellData.getValue().getFechaInicioProperty().asString());
+      //  colTermina.setCellValueFactory(cellData -> cellData.getValue().getFechaFinProperty().asString());
+    //    colPuntaje.setCellValueFactory(cellData -> cellData.getValue().getPuntajeProperty().asString());
+        
+        entregas = FXCollections.observableArrayList();
+        tbldocumentosEntregados.setItems(entregas);
+    }
+    
+    private void cargarEntregas() {
+        try {
+            List<Entrega> todasLasEntregas = new ArrayList<>();
+            
+            // Load all types of entregas
+            todasLasEntregas.addAll(EntregaDocumentoFinalDAO.obtenerEntregasPorExperienciaEducativa(experienciaEducativa.getId()));
+            todasLasEntregas.addAll(EntregaDocumentoInicialDAO.obtenerEntregasPorExperienciaEducativa(experienciaEducativa.getId()));
+            todasLasEntregas.addAll(EntregaDocumentoIntermedioDAO.obtenerEntregasPorExperienciaEducativa(experienciaEducativa.getId()));
+            todasLasEntregas.addAll(EntregaReporteDAO.obtenerEntregasPorExperienciaEducativa(experienciaEducativa.getId()));
+            
+            entregas.addAll(todasLasEntregas);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            VentanasUtils.mostrarAlertaSimple(Alert.AlertType.WARNING,
+                    ConstantesUtils.TITULO_ERROR,
+                    ConstantesUtils.ALERTA_ERROR_BD
+            );
+        }
+    }
 
     @FXML
     private void clicBtnCancelar(ActionEvent event) {
+        VentanasUtils.cerrarVentana(lblTitulo);
     }
-    
 }
